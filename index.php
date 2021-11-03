3788e57fa9b66f2aa90c2378bb97512d9ba336c5
Arthaud Proust <arthaudp33@gmail.com>
2021-11-03 21:23:59 +0100

    v2

--- index.php
+++ index.php
@@ -1,24 +1,60 @@
 <?php
+$path = './files/'.$_GET["path"].'/';
+$extList = file("extList.txt", FILE_IGNORE_NEW_LINES);
 $files = array();
+$desc = 'Cliquez pour voir les fichiers disponibles';
 
-if ($handle = opendir('./files')) {
+if(is_dir($path) && $_GET["path"]!='') {
+    if ($handle = opendir($path)) {
 
-    while (false !== ($entry = readdir($handle))) {
-
-        if ($entry != "." && $entry != ".." && $entry != "index.php") {
-            array_push($files, $entry);
+        while (false !== ($entry = readdir($handle))) {
+    
+            if ($entry != "." && $entry != ".." && $entry != "index.php") {
+                array_push($files, $entry);
+            }
         }
+        closedir($handle);
+
+        if(count($files) == 0) {
+            $desc = 'Aucun fichier';
+        } else if(count($files) == 1) {
+            $desc = '1 fichier, '.$files[0];
+        } else {
+            $desc = count($files).' fichiers';
+            foreach($files as $file) {
+                $desc.= ", ".$file;
+            }
+        } 
+    } else {
+        $desc = 'Dossier inaccessible';
     }
-    closedir($handle);
+} else {
+    $desc = 'Lien invalide';
 }
 
 
+
 function size($file, $decimals = 0) {
-    $bytes = filesize('./files/'.$file);
+    $bytes = filesize($GLOBALS['path'].$file);
     $factor = floor((strlen($bytes) - 1) / 3);
     if ($factor > 0) $sz = 'KMGT';
     return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . @$sz[$factor - 1] . 'o';
 }
+
+function getLink($file) {
+    $info = new SplFileInfo($file);
+    if (in_array($info->getExtension(), $GLOBALS['extList'])) {
+        return('<li class="file"><label><input type="checkbox" checked data-file="'.$GLOBALS['path'].$file.'">'.$file.'  ('.size($file).')</label><a class="downFile" data-method="js">Télécharger</a></li>');
+    } else {
+        return('<li class="file"><label><input type="checkbox" checked data-file="'.$GLOBALS['path'].$file.'">'.$file.'  ('.size($file).')</label><a href="'.$GLOBALS['path'].$file.'" download="'.$file.'" class="downFile" data-method="html">Télécharger</a></li>');
+    }
+
+}
+
+
+
+
+
 ?>
 
 
@@ -27,22 +63,58 @@ function size($file, $decimals = 0) {
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Télécharger</title>
+        <title>Télécharger les fichiers</title>
         <script src="/res/jquery-min.js" defer></script>
         <script src="/res/meta.js" defer></script>
         <link rel="stylesheet" href="/res/styles.css">
+
+        <meta name=target content=noindex>
+        <meta name=robots content=noindex>
+        <meta name=theme-color content=#212121>
+
+        <meta name=description content="<?=$desc ?>">
+        <meta name=author content="Arthaud PROUST">
+        <meta name=reply-to content=contact@arthaud.dev>
+        <meta name=subject content=developpement>
+        <meta name=language content=FR>
+        <meta name=owner content="Arthaud PROUST">
+        <meta name=url content=https://file.arthaud.dev>
+        <meta name=identifier-URL content=https://file.arthaud.dev>
+        
+        <meta property=og:title content="Télécharger les fichiers">
+        <meta property=og:type content=website>
+        <meta property=og:description content="<?=$desc ?>">
+        <meta property=og:site_name content=file.arthaud.dev>
+        <meta property=og:url content=https://file.arthaud.dev>
+        <meta property=og:locale content=fr_FR>
+        <meta property=og:image content=https://arthaud.dev/img/flask_2048_min.webp>
+
+        <meta name=twitter:card content=summary>
+        <meta name=twitter:title content="Télécharger les fichiers">
+        <meta name=twitter:site content=file.arthaud.dev>
+        <meta name=twitter:description content="<?=$desc ?>">
+        <meta name=twitter:image content=https://arthaud.dev/img/flask_2048_min.png>
+        <meta name=twitter:image:alt content="Télécharger les fichiers">
+
+        <meta name=apple-mobile-web-app-capable content=yes>
+        <meta name=apple-mobile-web-app-status-bar-style content=#212121>
+        <meta name=apple-mobile-web-app-title content="Télécharger les fichiers">
+
+        <link rel=canonical href=https://file.arthaud.dev/ >
     </head>
     <body>
-    
     <?php
-    if(count($files) == 0) {
+    if(!is_dir($path) || $_GET["path"]=='') {
+        echo('<h3>Lien invalide</h3>');
+    } else if(count($files) == 0) {
         echo('<h3>Aucun fichier</h3>');
     } else if(count($files) == 1) {
         echo('<h3>1 fichier</h3>
-        <ul id="files">
-            <li class="file"><label><input type="checkbox" checked data-file="'.$files[0].'">'.$files[0].'  ('.size($files[0]).')</label><button class="downFile ">Télécharger</button></li>
-        </ul>
+        <ul id="files">'.
+            getLink($files[0])
+        .'</ul>
         ');
+        //<li class="file"><label><input type="checkbox" checked data-file="'.$GLOBALS['path'].$files[0].'">'.$files[0].'  ('.size($files[0]).')</label><button class="downFile ">Télécharger</button></li>
     } else {
         echo('
             <h1>'.count($files).' fichiers</h1>
@@ -50,7 +122,7 @@ function size($file, $decimals = 0) {
             <li class="headbar"><label><input type="checkbox" checked class="selectAll">Tout sélectionner</label><button class="downAll">Tout télécharger</button></li>
             ');
         foreach($files as $file) {
-            echo('<li class="file"><label><input type="checkbox" checked data-file="'.$file.'">'.$file.'  ('.size($file).')</label><button class="downFile ">Télécharger</button></li>');
+            echo(getLink($file));
         }
         if(count($files) > 7) {
             echo('<li class="headbar"><label><input type="checkbox" checked class="selectAll">Tout sélectionner</label><button class="downAll">Tout télécharger</button></li>');
--- res/meta.js
+++ res/meta.js
@@ -16,7 +16,7 @@ $(()=>{
             .catch(e=>{$('#log').html('Certains fichiers n\'ont pas pu être téléchargé');$(this).attr('class', 'downAll error')});
     });
 
-    $(".downFile").click(function() {
+    $(".downFile[data-method='js']").click(function() {
         let btn = $(this);
         btn.attr('class', 'downFile loading');
         download($(this).prev().find('input').attr('data-file'))
@@ -24,13 +24,18 @@ $(()=>{
             .catch(()=> btn.attr('class', 'downFile error'));
     });
 
+    $(".downFile[data-method='html']").click(function() {
+        $(this).attr('class', 'downFile success');
+    });
+
 
     function downloadAll() {     
         return new Promise ((resolve, reject) => {
             $('#files').find(".file input:checked").each(function(index){
-                let btn = $(this).parent().next('button.downFile');
-            
-                download($(this).attr('data-file'))
+                let btn = $(this).parent().next('a.downFile');
+                
+                if(btn.attr('data-method') == "js") {
+                    download($(this).attr('data-file'))
                     .then(() => {
                         btn.attr('class', 'downFile success');
                         
@@ -39,14 +44,20 @@ $(()=>{
                         }
                     })
                     .catch(()=> {btn.attr('class', 'downFile error'); reject()});
+                } else {
+                    $(this).parent().next('a.downFile')[0].click();
+                    resolve();
+                }                
             });
         });
     }
 
     function download(file) {
         return new Promise((resolve, reject) => {
-            fetch('/files/'+file)
+            fetch(file)
             .then(resp => {
+                console.log(resp);
+                
                 if(resp.status == 404) {
                     reject();
                 } else {
@@ -55,7 +66,7 @@ $(()=>{
                         const a = document.createElement('a');
                         a.style.display = 'none';
                         a.href = url;
-                        a.download = file;
+                        a.download = file.split('/')[3];
                         document.body.appendChild(a);
                         a.click();
                         window.URL.revokeObjectURL(url);
